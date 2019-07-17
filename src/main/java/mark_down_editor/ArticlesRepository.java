50864280221b7cb3862a430aea131b9b2c845645
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-07-17 22:50:08 +0900

    new file: src/main/java/mark_down_editor/ArticlesRepository.java

---
+++ src/main/java/mark_down_editor/ArticlesRepository.java
@@ -0,0 +1,18 @@
+package mark_down_editor;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 記事の検索と登録・更新・削除を管理する。
+ */
+@Repository
+public interface ArticlesRepository extends JpaRepository<Articles, Long> {
+    /**
+     * @param id 検索対象とする記事の id を指定する。
+     * @return article 指定した id を保持する Articles エンティティを返す。
+     */
+    public Optional<Articles> findById(long id);
+}
