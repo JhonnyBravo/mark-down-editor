cac0ba4b44563c229de4485449402fe3658b2570
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-07-17 22:51:13 +0900

    new file: src/main/java/mark_down_editor/MarkDownEditorApplication.java

---
+++ src/main/java/mark_down_editor/MarkDownEditorApplication.java
@@ -0,0 +1,16 @@
+package mark_down_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * アプリケーションを起動する。
+ */
+@SpringBootApplication
+public class MarkDownEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkDownEditorApplication.class, args);
+    }
+
+}
