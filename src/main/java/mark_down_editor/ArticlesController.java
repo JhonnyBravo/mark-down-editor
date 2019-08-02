38b9be2e9cd8a8c8c3230e80f1bb20eb9073dc2d
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-02 21:57:50 +0900

    modified: src/main/java/mark_down_editor/ArticlesController.java
    thymeleaf 用の処理を削除。
    REST API として機能するように修正。

--- src/main/java/mark_down_editor/ArticlesController.java
+++ src/main/java/mark_down_editor/ArticlesController.java
@@ -1,126 +1,61 @@
 package mark_down_editor;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 記事の閲覧と登録・更新・削除を実行する。
  */
-@Controller
+@RestController
+@RequestMapping("/card")
 public class ArticlesController {
     @Autowired
     private ArticlesRepository repository;
-    private Link brand;
-    private List<Link> links;
-
-    public ArticlesController() {
-        this.brand = new Link("Mark Down Editor", "/");
-        this.links = new ArrayList<Link>();
-
-        this.links.add(new Link("新規作成", "/create"));
-    }
 
     /**
-     * @param mav ModelAndView を注入する。
-     * @return ModelAndView 登録されている全記事を一覧表示する。
+     * @return List&lt;Articles&gt; 登録されている全記事の一覧を JSON 形式で返す。
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView doGetIndex(ModelAndView mav) {
-        mav.setViewName("index");
-
-        mav.addObject("brand", brand);
-        mav.addObject("links", links);
-
+    @RequestMapping(value = "/view", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Articles> doGetCard() {
         List<Articles> cards = repository.findAll();
-        mav.addObject("cards", cards);
-
-        return mav;
+        return cards;
     }
 
     /**
-     * @param article 登録フォームへ渡す Articles エンティティを注入する。
-     * @param mav     ModelAndView を注入する。
-     * @return ModelAndView 記事の登録フォームを表示する。
+     * @param id 更新対象とする記事の id を指定する。
+     * @return Optional&lt;Articles&gt; 条件に合致する記事を取得し、 JSON 形式で返す。
      */
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public ModelAndView doGetCreate(@ModelAttribute Articles article, ModelAndView mav) {
-        mav.setViewName("edit_record");
-
-        mav.addObject("brand", brand);
-        mav.addObject("links", links);
-        mav.addObject("article", article);
-
-        return mav;
-    }
-
-    /**
-     * @param id      更新対象とする記事の id を指定する。
-     * @param article 更新フォームへ渡す Articles エンティティを注入する。
-     * @param mav     ModelAndView を注入する。
-     * @return ModelAndView 記事の更新フォームを表示する。
-     */
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
-    public ModelAndView doGetUpdate(@PathVariable long id, @ModelAttribute Articles article, ModelAndView mav) {
-        mav.setViewName("edit_record");
-        mav.addObject("brand", brand);
-        mav.addObject("links", links);
-
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public Optional<Articles> doGetUpdate(@PathVariable long id) {
         Optional<Articles> data = repository.findById(id);
-        mav.addObject("article", data.get());
-
-        return mav;
-    }
-
-    /**
-     * @param id      削除対象とする記事の id を指定する。
-     * @param article 登録フォームへ渡す Articles エンティティを注入する。
-     * @param mav     ModelAndView を注入する。
-     * @return ModelAndView 記事の削除フォームを表示する。
-     */
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public ModelAndView doGetDelete(@PathVariable long id, @ModelAttribute Articles article, ModelAndView mav) {
-        mav.setViewName("delete_record");
-        mav.addObject("brand", brand);
-        mav.addObject("links", links);
-
-        Optional<Articles> data = repository.findById(id);
-        mav.addObject("article", data.get());
-
-        return mav;
+        return data;
     }
 
     /**
      * @param article 登録または更新の対象とする Articles エンティティを指定する。
-     * @param mav     ModelAndView を注入する。
-     * @return ModelAndView 記事の一覧ページへリダイレクトする。
      */
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
     @Transactional(readOnly = false)
-    public ModelAndView doPostEdit(@ModelAttribute Articles article, ModelAndView mav) {
+    public void doPostEdit(@RequestBody Articles article) {
         repository.saveAndFlush(article);
-        return new ModelAndView("redirect:/");
     }
 
     /**
-     * @param id  削除対象とする記事の id を指定する。
-     * @param mav ModelAndView を注入する。
-     * @return ModelAndView 記事の一覧ページへリダイレクトする。
+     * @param article 削除対象とする Articles エンティティを指定する。
      */
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     @Transactional(readOnly = false)
-    public ModelAndView doPostDelete(@RequestParam long id, ModelAndView mav) {
-        repository.deleteById(id);
-        return new ModelAndView("redirect:/");
+    public void doPostDelete(@RequestBody Articles article) {
+        repository.delete(article);
     }
 }
