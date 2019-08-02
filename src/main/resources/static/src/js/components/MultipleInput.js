09be997c8ed50586793054570532e95abe4b01a6
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-02 21:49:25 +0900

    new file: src/main/resources/static/src/js/components/MultipleInput.js

---
+++ src/main/resources/static/src/js/components/MultipleInput.js
@@ -0,0 +1,19 @@
+export default {
+    template: `
+    <div>
+        <label :for="id">{{label}}</label>
+        <textarea class="form-control" :id="id" :placeholder="placeholder" :value="value" v-on:input="$emit('input',$event.target.value)" :rows="rows"></textarea>
+    </div>
+    `,
+    model: {
+        prop: "value",
+        event: "input"
+    },
+    props: [
+        "id",
+        "label",
+        "placeholder",
+        "value",
+        "rows"
+    ]
+}
