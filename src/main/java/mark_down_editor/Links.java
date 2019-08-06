8a0c3873fa3b07e52fc4d005b4b91252c810414f
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-06 21:12:38 +0900

    modified: src/main/java/mark_down_editor/Links.java
    javadoc を追記。

--- src/main/java/mark_down_editor/Links.java
+++ src/main/java/mark_down_editor/Links.java
@@ -3,6 +3,9 @@ package mark_down_editor;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * リンクデータを管理する。
+ */
 public class Links {
     private Link brand;
     private List<Link> links;
@@ -11,18 +14,30 @@ public class Links {
         this.links = new ArrayList<Link>();
     }
 
+    /**
+     * @return brand navbar へ渡す brand オブジェクトを返す。
+     */
     public Link getBrand() {
         return brand;
     }
 
+    /**
+     * @param brand brand オブジェクトとして設定する Link オブジェクトを指定する。
+     */
     public void setBrand(Link brand) {
         this.brand = brand;
     }
 
+    /**
+     * @return links navbar へ渡すリンクデータのリストを返す。
+     */
     public List<Link> getLinks() {
         return links;
     }
 
+    /**
+     * @param link リンクデータのリストへ登録する Link オブジェクトを指定する。
+     */
     public void addLinks(Link link) {
         this.links.add(link);
     }
