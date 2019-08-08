d59b34f8fdcf9cb105d91b670868aa74a704ffc0
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-09 03:04:31 +0900

    new file: src/main/resources/static/src/js/components/SingleInput.js

---
+++ src/main/resources/static/src/js/components/SingleInput.js
@@ -0,0 +1,18 @@
+export default {
+    template: `
+    <div>
+        <label :for="id">{{label}}</label>
+        <input type="text" class="form-control" :id="id" :name="id" :placeholder="placeholder" :value="value" v-on:input="$emit('input',$event.target.value)">
+    </div>
+    `,
+    model: {
+        prop: "value",
+        event: "input"
+    },
+    props: [
+        "id",
+        "label",
+        "placeholder",
+        "value"
+    ]
+}
