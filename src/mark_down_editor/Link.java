cb99bb942c01dc087dd177c1065cd587a0666598
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-15 03:09:08 +0900

    new file: src/mark_down_editor/Link.java

---
+++ src/mark_down_editor/Link.java
@@ -0,0 +1,32 @@
+package mark_down_editor;
+
+/**
+ * リンクデータを管理する。
+ */
+public class Link {
+    private String title;
+    private String url;
+
+    /**
+     * @param title リンクラベルとして設定する文字列を指定する。
+     * @param url   URL として設定する文字列を指定する。
+     */
+    public Link(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    /**
+     * @return title リンクラベルを返す。
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return url URL を返す。
+     */
+    public String getUrl() {
+        return url;
+    }
+}
