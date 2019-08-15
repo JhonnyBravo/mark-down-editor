31602a335819be1fcfe2223c2ecb861ecb2dcafb
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-08-15 20:32:41 +0900

    new file: WebContent/src/js/pages/DeleteRecord.js

---
+++ WebContent/src/js/pages/DeleteRecord.js
@@ -0,0 +1,25 @@
+import Rest from "../mixins/Rest";
+
+export default {
+    template: `
+    <div>
+        <div class="alert alert-danger text-center">記事を削除します。よろしいですか?</div>
+        <form>
+            <input type="hidden" v-model="id">
+            <button type="button" class="btn btn-danger btn-block" v-on:click="deleteById">Delete</button>
+            <router-link class="btn btn-secondary btn-block" to="/">Cancel</router-link>
+        </form>
+    </div>
+    `,
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    mixins: [Rest],
+    methods: {
+        deleteById() {
+            this.deleteData(`/mark-down-editor/articles`, this);
+        }
+    }
+}
