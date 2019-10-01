3e90bf8f1e0ade4ea9ec48f02f7ec18bc955564e
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-10-01 21:05:18 +0900

    modified: src/mark_down_editor/ArticlesController.java
    DBController の仕様変更に伴うコード修正。

--- src/mark_down_editor/ArticlesController.java
+++ src/mark_down_editor/ArticlesController.java
@@ -2,6 +2,7 @@ package mark_down_editor;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
@@ -12,42 +13,54 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+import property_resource.PropertyResource;
+
 /**
  * 記事の管理を行う為の REST API コントローラ。
  */
 @WebServlet("/articles")
 public class ArticlesController extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private DBController dbc = null;
-    private Gson gson = null;
+    private final DBController dbc;
+    private final Gson gson;
 
     /**
+     * @throws Exception {@link java.lang.Exception}
      * @see HttpServlet#HttpServlet()
      */
-    public ArticlesController() {
+    public ArticlesController() throws Exception {
         super();
-        dbc = new DBController("WebContent/WEB-INF/classes/connection.properties");
+        final PropertyResource pr = new PropertyResource("WebContent/WEB-INF/classes/connection.properties");
+        final Map<String, String> properties = pr.getContent();
+
+        dbc = new DBController(properties);
         gson = new Gson();
     }
 
     /**
      * DB に登録されている記事を JSON 形式でクライアントへ送信する。
-     * 
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     *
+     * @param request  REST クライアントから送信された Get パラメータ
+     * @param response REST クライアントへ返す JSON データ
+     * @throws ServletException {@link javax.servlet.ServletException}
+     * @throws IOException      {@link java.io.Exception}
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String json = null;
 
-        if (request.getParameterMap().containsKey("id")) {
-            long id = Long.parseLong(request.getParameter("id"));
-            Articles articles = dbc.findById(id);
-            json = gson.toJson(articles);
-        } else {
-            List<Articles> articles = dbc.findAll();
-            json = gson.toJson(articles);
+        try {
+            if (request.getParameterMap().containsKey("id")) {
+                final long id = Long.parseLong(request.getParameter("id"));
+                final Articles articles = dbc.findById(id);
+                json = gson.toJson(articles);
+            } else {
+                final List<Articles> articles = dbc.findAll();
+                json = gson.toJson(articles);
+            }
+        } catch (final Exception e) {
+            throw new IOException(e);
         }
 
         response.setContentType("application/json");
@@ -58,39 +71,65 @@ public class ArticlesController extends HttpServlet {
     /**
      * 記事を DB へ新規登録する。
      * 
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @param request  REST クライアントから送信された JSON データ
+     * @param response REST クライアントへ返すステータス(未実装)
+     * @throws ServletException {@link javax.servlet.ServletException}
+     * @throws IOException      {@link java.io.Exception}
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String json = request.getReader().lines().collect(Collectors.joining("\r\n"));
-        Articles articles = gson.fromJson(json, Articles.class);
-        dbc.create(articles);
+        final String json = request.getReader().lines().collect(Collectors.joining("\r\n"));
+        final Articles articles = gson.fromJson(json, Articles.class);
+
+        try {
+            dbc.create(articles);
+        } catch (final Exception e) {
+            throw new IOException(e);
+        }
     }
 
     /**
      * DB に登録されている既存記事を更新する。
-     * 
-     * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+     *
+     * @param request  REST クライアントから送信された JSON データ
+     * @param response REST クライアントへ返すステータス(未実装)
+     * @throws ServletException {@link javax.servlet.ServletException}
+     * @throws IOException      {@link java.io.Exception}
      */
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String json = request.getReader().lines().collect(Collectors.joining("\r\n"));
-        Articles articles = gson.fromJson(json, Articles.class);
-        dbc.updateById(articles);
+        final String json = request.getReader().lines().collect(Collectors.joining("\r\n"));
+        final Articles articles = gson.fromJson(json, Articles.class);
+
+        try {
+            dbc.update(articles);
+        } catch (final Exception e) {
+            throw new IOException(e);
+        }
     }
 
     /**
      * DB に登録されている記事を削除する。
      * 
-     * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+     * @param request  REST クライアントから送信された JSON データ
+     * @param response REST クライアントへ返すステータス(未実装)
+     * @throws ServletException {@link javax.servlet.ServletException}
+     * @throws IOException      {@link java.io.Exception}
      */
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        long id = Long.parseLong(request.getParameter("id"));
-        dbc.deleteById(id);
+        final Articles articles = new Articles();
+
+        final long id = Long.parseLong(request.getParameter("id"));
+        articles.setId(id);
+
+        try {
+            dbc.delete(articles);
+        } catch (final Exception e) {
+            throw new IOException(e);
+        }
     }
 }
