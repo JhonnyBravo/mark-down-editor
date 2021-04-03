8daee65f2e12ab8cd0ac80b61e3a603aaff39eb7
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-04-03 10:53:02 +0900

    renamed: src/main/java/mark_down_editor/{ArticlesRepository.java -> domain/repository/articles/ArticlesRepository.java}

--- src/main/java/mark_down_editor/domain/repository/articles/ArticlesRepository.java
+++ src/main/java/mark_down_editor/domain/repository/articles/ArticlesRepository.java
@@ -1,9 +1,11 @@
-package mark_down_editor;
+package mark_down_editor.domain.repository.articles;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import mark_down_editor.domain.model.Articles;
 
 /**
  * 記事の検索と登録・更新・削除を管理する。
