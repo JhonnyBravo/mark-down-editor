27fd93c6b599dd14cda0c6d58902a0cad0507189
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-18 23:08:57 +0900

    modified: src/main/java/mark_down_editor/app/articles/ArticlesHelper.java
    checkstyle の指摘事項を修正。
    
    プロパティファイルの読込みができなくなってしまった為、
    ConnectionInfoService を廃止。
    クラスローダーを利用して読込むように修正。

--- src/main/java/mark_down_editor/app/articles/ArticlesHelper.java
+++ src/main/java/mark_down_editor/app/articles/ArticlesHelper.java
@@ -1,39 +1,46 @@
 package mark_down_editor.app.articles;
 
+import java.util.Properties;
 import java_itamae_connection.domain.model.ConnectionInfo;
-import java_itamae_connection.domain.service.connection_info.ConnectionInfoService;
-import java_itamae_connection.domain.service.connection_info.ConnectionInfoServiceImpl;
-import java_itamae_contents.domain.model.ContentsAttribute;
 import mark_down_editor.domain.service.articles.ArticlesService;
 import mark_down_editor.domain.service.articles.ArticlesServiceImpl;
 
+/**
+ * {@link ArticlesService} の生成を管理する。
+ */
 public class ArticlesHelper {
-    private final ConnectionInfoService cis;
+  /**
+   * connection.properties から DB の接続情報を読込み、 {@link ConnectionInfo} に変換して返す。
+   *
+   * @param path connection.properties のパスを指定する。
+   * @return connectionInfo {@link ConnectionInfo} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  private ConnectionInfo getConnectionInfo(String path) throws Exception {
+    final Properties properties = new Properties();
+    properties.load(this.getClass().getClassLoader().getResourceAsStream(path));
 
-    public ArticlesHelper() {
-        cis = new ConnectionInfoServiceImpl();
-    }
+    final ConnectionInfo cnInfo = new ConnectionInfo();
+    cnInfo.setHostName(properties.getProperty("hostName"));
+    cnInfo.setPortNumber(properties.getProperty("portNumber"));
+    cnInfo.setDbName(properties.getProperty("dbName"));
+    cnInfo.setUserName(properties.getProperty("userName"));
+    cnInfo.setPassword(properties.getProperty("password"));
+    cnInfo.setEncoding(properties.getProperty("encoding"));
+    cnInfo.setTimeZone(properties.getProperty("timeZone"));
 
-    /**
-     * @param path DB の接続情報を記述した設定ファイルのパスを指定する。
-     * @return connectionInfo DB の接続情報を読込み、 ConnectionInfo に変換して返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    private ConnectionInfo getConnectionInfo(String path) throws Exception {
-        final ContentsAttribute attr = new ContentsAttribute();
-        attr.setPath(path);
+    return cnInfo;
+  }
 
-        final ConnectionInfo connectionInfo = cis.getConnectionInfo(attr);
-        return connectionInfo;
-    }
-
-    /**
-     * @param path DB の接続情報を記述した設定ファイルのパスを指定する。
-     * @return articlesService ArticlesService のインスタンスを返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public ArticlesService getArticlesService(String path) throws Exception {
-        final ArticlesService service = new ArticlesServiceImpl(getConnectionInfo(path));
-        return service;
-    }
+  /**
+   * connection.properties から DB の接続情報を読込み、 {@link ArticlesService} を生成する。
+   *
+   * @param path connection.properties のパスを指定する。
+   * @return articlesService {@link ArticlesService} のインスタンスを返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public ArticlesService getArticlesService(String path) throws Exception {
+    final ArticlesService service = new ArticlesServiceImpl(getConnectionInfo(path));
+    return service;
+  }
 }
