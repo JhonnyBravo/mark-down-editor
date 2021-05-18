9192476ff4c53a430a6e2bad7c02e5a32e8ac5a5
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-18 22:56:12 +0900

    modified: src/main/java/mark_down_editor/domain/model/Articles.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/model/Articles.java
+++ src/main/java/mark_down_editor/domain/model/Articles.java
@@ -4,49 +4,61 @@ package mark_down_editor.domain.model;
  * 記事を永続化する為のエンティティ。
  */
 public class Articles {
-    private long id;
-    private String title;
-    private String contents;
+  private long id;
+  private String title;
+  private String contents;
 
-    /**
-     * @param id id として登録する数値を指定する。
-     */
-    public void setId(long id) {
-        this.id = id;
-    }
+  /**
+   * 記事の ID を設定する。
+   *
+   * @param id ID として登録する数値を指定する。
+   */
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    /**
-     * @return id 記事の id を返す。
-     */
-    public long getId() {
-        return id;
-    }
+  /**
+   * 記事の ID を返す。
+   *
+   * @return id 記事の ID
+   */
+  public long getId() {
+    return id;
+  }
 
-    /**
-     * @param title 記事のタイトルとして登録する文字列を指定する。
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  /**
+   * 記事のタイトルを設定する。
+   *
+   * @param title 記事のタイトルとして登録する文字列を指定する。
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    /**
-     * @return 記事のタイトルを返す。
-     */
-    public String getTitle() {
-        return title;
-    }
+  /**
+   * 記事のタイトルを返す。
+   *
+   * @return 記事のタイトル
+   */
+  public String getTitle() {
+    return title;
+  }
 
-    /**
-     * @param contents 記事の本文として登録する文字列を指定する。
-     */
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
+  /**
+   * 記事の本文を設定する。
+   *
+   * @param contents 記事の本文として登録する文字列を指定する。
+   */
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
 
-    /**
-     * @return contents 記事の本文を返す。
-     */
-    public String getContents() {
-        return contents;
-    }
+  /**
+   * 記事の本文を返す。
+   *
+   * @return contents 記事の本文
+   */
+  public String getContents() {
+    return contents;
+  }
 }
