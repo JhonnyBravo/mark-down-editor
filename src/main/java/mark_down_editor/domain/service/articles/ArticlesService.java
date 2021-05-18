250d0993c879fb33ccc3d46fa72c0480ccb51759
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-18 23:03:49 +0900

    modified: src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
+++ src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
@@ -1,56 +1,58 @@
 package mark_down_editor.domain.service.articles;
 
 import java.util.List;
-
 import java_itamae_connection.domain.service.connection.ConnectionService;
 import mark_down_editor.domain.model.Articles;
 
+/**
+ * articles テーブルへのレコード入出力を管理する。
+ */
 public abstract class ArticlesService extends ConnectionService {
-    /**
-     * テーブルからレコードを全件取得し、 List に変換して返す。
-     *
-     * @return recordset 変換された List
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract List<Articles> findAll() throws Exception;
+  /**
+   * テーブルからレコードを全件取得し、 {@link Articles} の {@link List} に変換して返す。
+   *
+   * @return recordset {@link Articles} の {@link List}
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract List<Articles> findAll() throws Exception;
 
-    /**
-     * ID をキーにレコードを検索し、取得する。
-     *
-     * @param id 検索対象とする記事の ID を指定する。
-     * @return record 指定した ID を持つレコードを返す。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract Articles findById(long id) throws Exception;
+  /**
+   * ID をキーにレコードを検索し、取得する。
+   *
+   * @param id 検索対象とする記事の ID を指定する。
+   * @return record 指定した ID を持つ {@link Articles} を返す。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract Articles findById(long id) throws Exception;
 
-    /**
-     * 記事を新規登録する。
-     *
-     * @param articles 登録対象とする Articles を指定する。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract void create(Articles articles) throws Exception;
+  /**
+   * 記事を新規登録する。
+   *
+   * @param articles 登録対象とする {@link Articles} を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract void create(Articles articles) throws Exception;
 
-    /**
-     * 既存の記事を更新する。
-     *
-     * @param articles 更新対象とする Articles を指定する。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract void update(Articles articles) throws Exception;
+  /**
+   * 既存の記事を更新する。
+   *
+   * @param articles 更新対象とする {@link Articles} を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract void update(Articles articles) throws Exception;
 
-    /**
-     * 記事を一件削除する。
-     *
-     * @param articles 削除対象とする Articles を指定する。
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract void delete(Articles articles) throws Exception;
+  /**
+   * 記事を一件削除する。
+   *
+   * @param articles 削除対象とする {@link Articles} を指定する。
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract void delete(Articles articles) throws Exception;
 
-    /**
-     * 既存の記事を全件削除する。
-     *
-     * @throws Exception {@link java.lang.Exception}
-     */
-    public abstract void deleteAll() throws Exception;
+  /**
+   * 既存の記事を全件削除する。
+   *
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public abstract void deleteAll() throws Exception;
 }
