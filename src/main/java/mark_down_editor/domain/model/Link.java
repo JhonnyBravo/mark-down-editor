d0fc836cb0a9774bc289b6a9c7eabe0732636226
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-18 22:57:32 +0900

    modified: src/main/java/mark_down_editor/domain/model/Link.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/model/Link.java
+++ src/main/java/mark_down_editor/domain/model/Link.java
@@ -4,29 +4,35 @@ package mark_down_editor.domain.model;
  * リンクデータを管理する。
  */
 public class Link {
-    private String title;
-    private String url;
+  private final String title;
+  private final String url;
 
-    /**
-     * @param title リンクラベルとして設定する文字列を指定する。
-     * @param url   URL として設定する文字列を指定する。
-     */
-    public Link(String title, String url) {
-        this.title = title;
-        this.url = url;
-    }
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param title リンクラベルとして設定する文字列を指定する。
+   * @param url URL として設定する文字列を指定する。
+   */
+  public Link(String title, String url) {
+    this.title = title;
+    this.url = url;
+  }
 
-    /**
-     * @return title リンクラベルを返す。
-     */
-    public String getTitle() {
-        return title;
-    }
+  /**
+   * リンクラベルを返す。
+   *
+   * @return title リンクラベル
+   */
+  public String getTitle() {
+    return title;
+  }
 
-    /**
-     * @return url URL を返す。
-     */
-    public String getUrl() {
-        return url;
-    }
+  /**
+   * URL を返す。
+   *
+   * @return url リンクの URL
+   */
+  public String getUrl() {
+    return url;
+  }
 }
