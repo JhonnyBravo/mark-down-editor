a26259bb16d074839dcfce72047787efdb8b49e7
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-18 23:04:15 +0900

    modified: src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
    checkstyle, pmd の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
+++ src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
@@ -3,104 +3,97 @@ package mark_down_editor.domain.service.articles;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
-
 import java_itamae_connection.domain.model.ConnectionInfo;
 import mark_down_editor.domain.model.Articles;
 import mark_down_editor.domain.repository.articles.ArticlesRepository;
 import mark_down_editor.domain.repository.articles.ArticlesRepositoryImpl;
 
 public class ArticlesServiceImpl extends ArticlesService {
-    private final ConnectionInfo cnInfo;
-    private final ArticlesRepository repository;
+  private final ConnectionInfo cnInfo;
+  private final ArticlesRepository repository;
 
-    /**
-     * @param cnInfo DB の接続情報情報を納めた ConnectionInfo を指定する。
-     */
-    public ArticlesServiceImpl(ConnectionInfo cnInfo) {
-        this.cnInfo = cnInfo;
-        repository = new ArticlesRepositoryImpl();
+  /**
+   * 初期化処理を実行する。
+   *
+   * @param cnInfo DB の接続情報情報を納めた {@link ConnectionInfo} を指定する。
+   */
+  public ArticlesServiceImpl(ConnectionInfo cnInfo) {
+    this.cnInfo = cnInfo;
+    repository = new ArticlesRepositoryImpl();
+  }
+
+  @Override
+  public List<Articles> findAll() throws Exception {
+    List<Articles> recordset = new ArrayList<>();
+
+    try (Connection connection = getConnection(cnInfo)) {
+      recordset = repository.findAll(connection);
     }
 
-    @Override
-    public List<Articles> findAll() throws Exception {
-        List<Articles> recordset = new ArrayList<>();
+    return recordset;
+  }
 
-        try (Connection connection = getConnection(cnInfo)) {
-            recordset = repository.findAll(connection);
-        }
+  @Override
+  public Articles findById(long id) throws Exception {
+    Articles record = new Articles();
 
-        return recordset;
+    try (Connection connection = getConnection(cnInfo)) {
+      record = repository.findById(connection, id);
     }
 
-    @Override
-    public Articles findById(long id) throws Exception {
-        Articles record = new Articles();
+    return record;
+  }
 
-        try (Connection connection = getConnection(cnInfo)) {
-            record = repository.findById(connection, id);
-        }
-
-        return record;
+  @Override
+  public void create(Articles articles) throws Exception {
+    try (final Connection connection = getConnection(cnInfo);) {
+      try {
+        connection.setAutoCommit(false);
+        repository.create(connection, articles);
+        connection.commit();
+      } catch (final Exception e) {
+        connection.rollback();
+      }
     }
+  }
 
-    @Override
-    public void create(Articles articles) throws Exception {
-        final Connection connection = getConnection(cnInfo);
-
-        try {
-            connection.setAutoCommit(false);
-            repository.create(connection, articles);
-            connection.commit();
-        } catch (final Exception e) {
-            connection.rollback();
-        } finally {
-            connection.close();
-        }
+  @Override
+  public void update(Articles articles) throws Exception {
+    try (final Connection connection = getConnection(cnInfo);) {
+      try {
+        connection.setAutoCommit(false);
+        repository.update(connection, articles);
+        connection.commit();
+      } catch (final Exception e) {
+        connection.rollback();
+      }
     }
+  }
 
-    @Override
-    public void update(Articles articles) throws Exception {
-        final Connection connection = getConnection(cnInfo);
-
-        try {
-            connection.setAutoCommit(false);
-            repository.update(connection, articles);
-            connection.commit();
-        } catch (final Exception e) {
-            connection.rollback();
-        } finally {
-            connection.close();
-        }
+  @Override
+  public void delete(Articles articles) throws Exception {
+    try (final Connection connection = getConnection(cnInfo);) {
+      try {
+        connection.setAutoCommit(false);
+        repository.delete(connection, articles);
+        connection.commit();
+      } catch (final Exception e) {
+        connection.rollback();
+      }
     }
+  }
 
-    @Override
-    public void delete(Articles articles) throws Exception {
-        final Connection connection = getConnection(cnInfo);
-
-        try {
-            connection.setAutoCommit(false);
-            repository.delete(connection, articles);
-            connection.commit();
-        } catch (final Exception e) {
-            connection.rollback();
-        } finally {
-            connection.close();
-        }
+  @Override
+  public void deleteAll() throws Exception {
+    try (final Connection connection = getConnection(cnInfo);) {
+      try {
+        connection.setAutoCommit(false);
+        repository.deleteAll(connection);
+        connection.commit();
+      } catch (final Exception e) {
+        connection.rollback();
+      }
     }
-
-    @Override
-    public void deleteAll() throws Exception {
-        final Connection connection = getConnection(cnInfo);
-
-        try {
-            connection.setAutoCommit(false);
-            repository.deleteAll(connection);
-            connection.commit();
-        } catch (final Exception e) {
-            connection.rollback();
-        } finally {
-            connection.close();
-        }
-    }
+  }
 
 }
