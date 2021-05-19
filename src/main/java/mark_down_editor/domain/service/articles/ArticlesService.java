4c48e33aa2be9df31460bfdff06ed5c0b595b593
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-19 23:45:09 +0900

    modified: src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
+++ src/main/java/mark_down_editor/domain/service/articles/ArticlesService.java
@@ -1,38 +1,44 @@
 package mark_down_editor.domain.service.articles;
 
 import java.util.List;
-
 import mark_down_editor.domain.model.Articles;
 
+/**
+ * 記事の検索、登録、更新、削除を管理する。
+ */
 public interface ArticlesService {
-    /**
-     * @return articles 登録されている全記事の一覧を取得し、 {@link List} に変換して返す。
-     */
-    List<Articles> findAll();
+  /**
+   * 登録されている全記事を取得する。
+   *
+   * @return articles {@link Articles} の {@link List}
+   */
+  List<Articles> findAll();
 
-    /**
-     * @param id 取得対象とする記事の id を指定する。
-     * @return articles 条件に合致する記事を取得して返す。
-     */
-    Articles findById(long id);
+  /**
+   * レコード ID をキーに記事を検索する。
+   *
+   * @param id 取得対象とする記事の id を指定する。
+   * @return articles 指定した ID を保持する {@link Articles}
+   */
+  Articles findById(long id);
 
-    /**
-     * 記事を登録または更新する。
-     *
-     * @param articles 登録または更新の対象とする {@link Articles} を指定する。
-     * @return articles 登録された記事を返す。
-     */
-    Articles edit(Articles articles);
+  /**
+   * 記事を登録または更新する。
+   *
+   * @param articles 登録または更新の対象とする {@link Articles} を指定する。
+   * @return articles 登録された {@link Articles}
+   */
+  Articles edit(Articles articles);
 
-    /**
-     * 記事を削除する。
-     *
-     * @param articles 削除対象とする {@link Articles} を指定する。
-     */
-    void delete(Articles articles);
+  /**
+   * 記事を削除する。
+   *
+   * @param articles 削除対象とする {@link Articles} を指定する。
+   */
+  void delete(Articles articles);
 
-    /**
-     * 登録されている全記事を削除する。
-     */
-    void deleteAll();
+  /**
+   * 登録されている全記事を削除する。
+   */
+  void deleteAll();
 }
