55a6ee885c6902a2f70e7d47ecbc20dc2c62ba67
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-19 23:43:49 +0900

    modified: src/main/java/mark_down_editor/domain/model/Links.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/model/Links.java
+++ src/main/java/mark_down_editor/domain/model/Links.java
@@ -7,38 +7,49 @@ import java.util.List;
  * リンクデータを管理する。
  */
 public class Links {
-    private Link brand;
-    private List<Link> links;
+  private Link brand;
+  private final List<Link> links;
 
-    public Links() {
-        this.links = new ArrayList<Link>();
-    }
+  /**
+   * 初期化処理を実行する。
+   */
+  public Links() {
+    this.links = new ArrayList<>();
+  }
 
-    /**
-     * @return brand navbar へ渡す brand オブジェクトを返す。
-     */
-    public Link getBrand() {
-        return brand;
-    }
+  /**
+   * navbar へ渡す brand オブジェクトを返す。
+   *
+   * @return brand {@link Link}
+   */
+  public Link getBrand() {
+    return brand;
+  }
 
-    /**
-     * @param brand brand オブジェクトとして設定する Link オブジェクトを指定する。
-     */
-    public void setBrand(Link brand) {
-        this.brand = brand;
-    }
+  /**
+   * brand オブジェクトを設定する。
+   *
+   * @param brand brand オブジェクトとして設定する {@link Link}
+   */
+  public void setBrand(Link brand) {
+    this.brand = brand;
+  }
 
-    /**
-     * @return links navbar へ渡すリンクデータのリストを返す。
-     */
-    public List<Link> getLinks() {
-        return links;
-    }
+  /**
+   * navbar へ渡すリンクデータのリストを返す。
+   *
+   * @return links {@link Link} の {@link List}
+   */
+  public List<Link> getLinks() {
+    return links;
+  }
 
-    /**
-     * @param link リンクデータのリストへ登録する Link オブジェクトを指定する。
-     */
-    public void addLinks(Link link) {
-        this.links.add(link);
-    }
+  /**
+   * リンクデータを登録する。
+   *
+   * @param link リンクデータのリストへ登録する {@link Link}
+   */
+  public void addLinks(Link link) {
+    this.links.add(link);
+  }
 }
