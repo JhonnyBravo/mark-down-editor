3b27cabfeccb3cf0a92af22f21bc1b5a8f0aac6f
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-19 23:45:53 +0900

    modified: src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
+++ src/main/java/mark_down_editor/domain/service/articles/ArticlesServiceImpl.java
@@ -1,48 +1,46 @@
 package mark_down_editor.domain.service.articles;
 
 import java.util.List;
-
+import mark_down_editor.domain.model.Articles;
+import mark_down_editor.domain.repository.articles.ArticlesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import mark_down_editor.domain.model.Articles;
-import mark_down_editor.domain.repository.articles.ArticlesRepository;
-
 @Service
 @Transactional
 public class ArticlesServiceImpl implements ArticlesService {
-    @Autowired
-    private ArticlesRepository repository;
+  @Autowired
+  private ArticlesRepository repository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<Articles> findAll() {
-        final List<Articles> result = repository.findAll();
-        return result;
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public List<Articles> findAll() {
+    final List<Articles> result = repository.findAll();
+    return result;
+  }
 
-    @Override
-    @Transactional(readOnly = true)
-    public Articles findById(long id) {
-        final Articles result = repository.findById(id).orElse(new Articles());
-        return result;
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public Articles findById(long id) {
+    final Articles result = repository.findById(id).orElse(new Articles());
+    return result;
+  }
 
-    @Override
-    public Articles edit(Articles articles) {
-        final Articles result = repository.saveAndFlush(articles);
-        return result;
-    }
+  @Override
+  public Articles edit(Articles articles) {
+    final Articles result = repository.saveAndFlush(articles);
+    return result;
+  }
 
-    @Override
-    public void delete(Articles articles) {
-        repository.delete(articles);
-    }
+  @Override
+  public void delete(Articles articles) {
+    repository.delete(articles);
+  }
 
-    @Override
-    public void deleteAll() {
-        repository.deleteAll();
-    }
+  @Override
+  public void deleteAll() {
+    repository.deleteAll();
+  }
 
 }
