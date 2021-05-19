400fdc8f4569f7737f4b17ae0d499ea12116ddf7
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-19 23:46:47 +0900

    modified: src/main/java/mark_down_editor/app/links/LinksController.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/app/links/LinksController.java
+++ src/main/java/mark_down_editor/app/links/LinksController.java
@@ -1,29 +1,36 @@
 package mark_down_editor.app.links;
 
+import mark_down_editor.domain.model.Link;
+import mark_down_editor.domain.model.Links;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import mark_down_editor.domain.model.Link;
-import mark_down_editor.domain.model.Links;
-
+/**
+ * navbar へ渡すリンクデータの一覧を管理する。
+ */
 @RestController
 public class LinksController {
-    private Links links;
+  private final Links links;
 
-    public LinksController() {
-        this.links = new Links();
-        this.links.setBrand(new Link("Mark Down Editor", "/"));
-        this.links.addLinks(new Link("New Article", "/edit"));
-    }
+  /**
+   * 初期化処理を実行する。
+   */
+  public LinksController() {
+    this.links = new Links();
+    this.links.setBrand(new Link("Mark Down Editor", "/"));
+    this.links.addLinks(new Link("New Article", "/edit"));
+  }
 
-    /**
-     * @return links navbar へ渡すリンクデータのリストを返す。
-     */
-    @RequestMapping(value = "/links", method = { RequestMethod.GET })
-    @ResponseBody
-    public Links getLinks() {
-        return links;
-    }
+  /**
+   * リンクデータの一覧を返す。
+   *
+   * @return links {@link Links}
+   */
+  @RequestMapping(value = "/links", method = {RequestMethod.GET})
+  @ResponseBody
+  public Links getLinks() {
+    return links;
+  }
 }
