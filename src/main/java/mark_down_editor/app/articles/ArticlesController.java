1a32ff9433f97978e7bff1654a60b1578588293a
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-19 23:47:35 +0900

    modified: src/main/java/mark_down_editor/app/articles/ArticlesController.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/app/articles/ArticlesController.java
+++ src/main/java/mark_down_editor/app/articles/ArticlesController.java
@@ -1,7 +1,8 @@
 package mark_down_editor.app.articles;
 
 import java.util.List;
-
+import mark_down_editor.domain.model.Articles;
+import mark_down_editor.domain.service.articles.ArticlesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,50 +11,55 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import mark_down_editor.domain.model.Articles;
-import mark_down_editor.domain.service.articles.ArticlesService;
-
 /**
  * 記事の閲覧と登録・更新・削除を実行する。
  */
 @RestController
 public class ArticlesController {
-    @Autowired
-    private ArticlesService service;
+  @Autowired
+  private ArticlesService service;
 
-    /**
-     * @return List&lt;Articles&gt; 登録されている全記事の一覧を JSON 形式で返す。
-     */
-    @RequestMapping(value = "/articles", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Articles> findAll() {
-        final List<Articles> articles = service.findAll();
-        return articles;
-    }
+  /**
+   * テーブルに登録されている全記事の一覧を取得する。
+   *
+   * @return articles {@link Articles} の {@link List}
+   */
+  @RequestMapping(value = "/articles", method = RequestMethod.GET)
+  @ResponseBody
+  public List<Articles> findAll() {
+    final List<Articles> articles = service.findAll();
+    return articles;
+  }
 
-    /**
-     * @param id 更新対象とする記事の id を指定する。
-     * @return Optional&lt;Articles&gt; 条件に合致する記事を取得し、 JSON 形式で返す。
-     */
-    @RequestMapping(value = "/articles/{id}", method = RequestMethod.GET)
-    public Articles findById(@PathVariable long id) {
-        final Articles articles = service.findById(id);
-        return articles;
-    }
+  /**
+   * ID をキーに記事を検索する。
+   *
+   * @param id 検索対象とする記事の id を指定する。
+   * @return articles {@link Articles}
+   */
+  @RequestMapping(value = "/articles/{id}", method = RequestMethod.GET)
+  public Articles findById(@PathVariable long id) {
+    final Articles articles = service.findById(id);
+    return articles;
+  }
 
-    /**
-     * @param article 登録または更新の対象とする Articles エンティティを指定する。
-     */
-    @RequestMapping(value = "/articles/edit", method = RequestMethod.POST)
-    public void edit(@RequestBody Articles article) {
-        service.edit(article);
-    }
+  /**
+   * 記事を新規登録または更新する。
+   *
+   * @param article 登録または更新の対象とする {@link Articles} を指定する。
+   */
+  @RequestMapping(value = "/articles/edit", method = RequestMethod.POST)
+  public void edit(@RequestBody Articles article) {
+    service.edit(article);
+  }
 
-    /**
-     * @param article 削除対象とする Articles エンティティを指定する。
-     */
-    @RequestMapping(value = "/articles/delete", method = RequestMethod.POST)
-    public void delete(@RequestBody Articles article) {
-        service.delete(article);
-    }
+  /**
+   * 記事を削除する。
+   *
+   * @param article 削除対象とする {@link Articles} を指定する。
+   */
+  @RequestMapping(value = "/articles/delete", method = RequestMethod.POST)
+  public void delete(@RequestBody Articles article) {
+    service.delete(article);
+  }
 }
