4b8f3d0a0f539cc1a37b4da878d1461d9aef3519
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-21 00:30:10 +0900

    modified: src/main/java/mark_down_editor/domain/repository/articles/ArticlesRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/mark_down_editor/domain/repository/articles/ArticlesRepository.java
+++ src/main/java/mark_down_editor/domain/repository/articles/ArticlesRepository.java
@@ -1,20 +1,20 @@
 package mark_down_editor.domain.repository.articles;
 
 import java.util.Optional;
-
+import mark_down_editor.domain.model.Articles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import mark_down_editor.domain.model.Articles;
 
 /**
  * 記事の検索と登録・更新・削除を管理する。
  */
 @Repository
 public interface ArticlesRepository extends JpaRepository<Articles, Long> {
-    /**
-     * @param id 検索対象とする記事の id を指定する。
-     * @return article 指定した id を保持する Articles エンティティを返す。
-     */
-    public Optional<Articles> findById(long id);
+  /**
+   * ID をキーに記事を検索する。
+   *
+   * @param id 検索対象とする記事の ID を指定する。
+   * @return article 指定した ID を保持する {@link Articles}
+   */
+  Optional<Articles> findById(long id);
 }
